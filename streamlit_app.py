465ab40060fa80f2e96dc117547d9ebe3fa5b02a
ulisesgarcia21346-crypto <ulisesgarcia21346@gmail.com>
2026-09-18 17:02:59 -0600

    Delete streamlit_app.py

--- streamlit_app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-st.title("🚀 Ulises IA 3 en 1 - Teziutlan")
-st.write("Creada por Ulises Vazquez")
-idea = st.text_input("¿Que imagen quieres crear?")
-if st.button("Crear imagen", type="primary"):
-    url = f"https://image.pollinations.ai/prompt/{idea}?nologo=true"
-    st.image(url)
-    st.success("¡Lista para vender en $200!")
